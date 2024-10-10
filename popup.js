975dbb16c32e8a0e875d6bcc3278b21672977248
Rebeca <rebeca.bispojb@gmail.com>
2024-10-10 17:22:17 -0300

    deixando mais bonito e facil

--- popup.js
+++ popup.js
@@ -5,6 +5,17 @@ document.getElementById('descriptografar').addEventListener('click', function() 
     if (!isNaN(numero)) {
         const fraseDescriptografada = descriptografar(frase, numero);
         document.getElementById('resultado').innerText = `Frase descriptografada: ${fraseDescriptografada}`;
+
+        // Copiar automaticamente para o clipboard
+        navigator.clipboard.writeText(fraseDescriptografada).then(() => {
+            // Mostrar mensagem de sucesso
+            document.getElementById('copiedMessage').style.display = 'block';
+            setTimeout(() => {
+                document.getElementById('copiedMessage').style.display = 'none';
+            }, 2000); // Esconde a mensagem após 2 segundos
+        }).catch(err => {
+            console.error('Erro ao copiar para o clipboard: ', err);
+        });
     } else {
         document.getElementById('resultado').innerText = "Por favor, insira um número válido.";
     }
